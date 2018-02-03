71074d39ba57e80bb7d819bcc2bbb976a0d550fc
vamsee <pasangulapativamsee@gmail.com>
2018-02-03 02:05:41 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+//route handler in express
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+//heroku will inject port into environment variable
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
